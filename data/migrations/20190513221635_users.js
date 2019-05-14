e0a73957bc43098328061d55e11c31878808041e
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-05-13 23:25:43 -0400

    re-structure users migration table, fix getAll users endpoint

--- data/migrations/20190513221635_users.js
+++ data/migrations/20190513221635_users.js
@@ -2,17 +2,11 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("users", function(users) {
     users.increments();
     users
-      .text("username", 25)
+      .text("email", 25)
       .notNullable()
       .unique();
-    users
-      .text("first_name", 25)
-      .notNullable()
-      .unique();
-    users
-      .text("last_name", 25)
-      .notNullable()
-      .unique();
+    users.text("first_name", 25).notNullable();
+    users.text("last_name", 25).notNullable();
   });
 };
 
--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,5 +1,6 @@
 const faker = require("faker");
 
+// Create Fake User Functions that returns an object (one user)
 const createFakeUser = () => ({
   email: faker.internet.email(),
   first_name: faker.name.firstName(),
@@ -7,9 +8,12 @@ const createFakeUser = () => ({
 });
 
 exports.seed = function(knex, Promise) {
+  // Create a new array of 50 faker generated users
   const users = [];
   for (let i = 0; i <= 50; i++) {
     users.push(createFakeUser());
   }
+
+  // Insert the array to users table
   return knex("users").insert(users);
 };
