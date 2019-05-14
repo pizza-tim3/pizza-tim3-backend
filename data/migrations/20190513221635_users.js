5981615ee9742127eb491f9a0af4c9c74752ee34
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-05-13 22:18:44 -0400

    add users migrations tables

---
+++ data/migrations/20190513221635_users.js
@@ -0,0 +1,14 @@
+exports.up = function(knex, Promise) {
+    return knex.schema
+    .createTable('roles', function(roles) {
+        roles.increments();
+        roles
+        .text('name', 25)
+        .notNullable()
+        .unique()
+    })  
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('roles');
+};
