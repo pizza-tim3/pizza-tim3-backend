903a5916998d3bdf8e754f58988b87ec9795f978
unknown <noah.stoeckel@gmail.com>
2019-05-20 22:06:28 -0400

    Change tables

--- data/migrations/20190515111950_LinkTable.js
+++ data/migrations/20190515111950_LinkTable.js
@@ -1,24 +1,22 @@
-
 exports.up = function(knex, Promise) {
-return knex.schema.createTable('links', function(links){
-links.increments();
-links.integer('event_id')//declaring a foreign key
-    .references('id')
-    .inTable('events');
+  return knex.schema.createTable("event_participants", function(
+    event_participants
+  ) {
+    event_participants.increments();
+    event_participants
+      .integer("event_participantsid") //declaring a foreign key
+      .references("id")
+      .inTable("events");
 
+    event_participants
+      .integer("user_id") //declaring the other foreign key
+      .references("id")
+      .inTable("users");
 
-links.integer('user_id')//declaring the other foreign key
-    .references('id')
-    .inTable('users');
-
-links.string("participants").notNullable();
+    event_participants.bool("accepted");
   });
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists('links');
-  
+  return knex.schema.dropTableIfExists("event_participants");
 };
-
-
-
