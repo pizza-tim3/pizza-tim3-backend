d42ced041c24a2f2c37fe63e6af01f14610fd3fd
unknown <noah.stoeckel@gmail.com>
2019-05-20 21:58:46 -0400

    Add friend id

--- data/migrations/20190515102553_FriendJoinTable.js
+++ data/migrations/20190515102553_FriendJoinTable.js
@@ -1,17 +1,21 @@
-
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('friends', function(friends){
-        friends.increments();
-            //foreign key
+  return knex.schema.createTable("friends", function(friends) {
+    friends.increments();
+    //foreign key
 
-        friends.integer('user_id')
-           .unsigned()
-           .references('id')
-           .inTable("users");
-           
-    })  
+    friends
+      .integer("user_id")
+      .unsigned()
+      .references("id")
+      .inTable("users");
+    friends
+      .integer("friend_id")
+      .unsigned()
+      .references("id")
+      .inTable("users");
+  });
 };
 
 exports.down = function(knex, Promise) {
- return knex.schema.dropTableIfExists('friends');
+  return knex.schema.dropTableIfExists("friends");
 };
