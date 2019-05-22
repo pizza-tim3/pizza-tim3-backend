74afe1ce2eea6110be519ac84aecc1a2b05bee6e
unknown <noah.stoeckel@gmail.com>
2019-05-21 21:59:21 -0400

    Add get users routes

--- api/users/userRoutes.js
+++ api/users/userRoutes.js
@@ -3,18 +3,23 @@ const router = express.Router();
 const Users = require("../../data/helpers/userDbHelper.js");
 // All Users route
 
-router.get("/", (req, res) => {
-  Users.getAll()
-    .then(users => {
-      if (users) {
-        res.status(200).json(users);
-      } else {
-        res.status(404).json("There are users");
-      }
-    })
-    .catch(err => {
-      res.status(500).json(`${err}`);
-    });
+router.get("/", async (req, res) => {
+  try {
+    const users = await Users.getAll();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await Users.getBy(id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
