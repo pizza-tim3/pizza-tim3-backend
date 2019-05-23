a4333f31a0e858b7fc7eb62a0af104b3bdc5bf9a
120356aa <yurlovandrew@gmail.com>
2019-05-22 21:13:18 -0700

    refactord and fixed bug

--- api/comments/comments.spec.js
+++ api/comments/comments.spec.js
@@ -2,11 +2,22 @@ const req = require('supertest');
 const server = require('../../server');
 const db = require("../../data/dbConfig");
 
-beforeEach(() => {
-  return db("comments").truncate();
-});
 
 describe("The Comments Router", () => {
+  beforeEach( async () => {
+    const res = await req(server)
+      .post('/api/comments/event')
+      .send({
+        id: 1,
+        time: "123",
+        message: "message",
+        event_id: 1,
+        user_id: 1,
+      });
+  });
+
+  afterEach( async () => await db('comments').delete());
+
   it('should start testing environment', () => {
     const res = process.env.DB_ENV;
     expect(res).toBe('testing');
@@ -14,57 +25,26 @@ describe("The Comments Router", () => {
 
   describe("GET /", () => {
     it("should return an array of comments", async () => {
-      const comments = [{
-        id: 1,
-        time: "1234",
-        message: "message",
-        event_id: 1,
-        user_id: 1,
-      }];
-
-      await db("comments").insert(comments);
       const res = await req(server).get("/api/comments");
 
       expect(res.status).toBe(200);
       expect(res.type).toBe("application/json");
       expect(res.body.length).toBe(1);
-      expect(res.body).toEqual([{
-        id: 1,
-        time: "1234",
-        message: "message",
-        event_id: 1,
-        user_id: 1,
-      }]);
     });
   });
 
   describe("GET /event_id", () => {
     it("should return comments by event_id", async () => {
-      const comments = [{
+      const res = await req(server).get("/api/comments/event/1");
+
+      expect(res.status).toBe(200);
+      expect(res.type).toBe("application/json");
+      expect(res.body).toEqual({
         id: 1,
         time: "123",
         message: "message",
         event_id: 1,
         user_id: 1,
-      },{
-        id: 2,
-        time: "145",
-        message: "message",
-        event_id: 3,
-        user_id: 4,
-      }];
-
-      await db("comments").insert(comments);
-      const res = await req(server).get("/api/comments/event/3");
-
-      expect(res.status).toBe(200);
-      expect(res.type).toBe("application/json");
-      expect(res.body).toEqual({
-        id: 2,
-        time: "145",
-        message: "message",
-        event_id: 3,
-        user_id: 4,
       });
     });
   });
@@ -74,11 +54,11 @@ describe("The Comments Router", () => {
       const res = await req(server)
         .post('/api/comments/event')
         .send({
-          id: 1,
+          id: 3,
           time: "123",
           message: "message",
-          event_id: 1,
-          user_id: 1,
+          event_id: 4,
+          user_id: 5,
         });
       expect(res.status).toBe(201);
     });
@@ -87,15 +67,6 @@ describe("The Comments Router", () => {
   describe("PUT /:id", () => {
     it("should update comment by id", async () => {
       const res = await req(server)
-        .post("/api/comments/event")
-        .send({
-          id: 1,
-          time: "123",
-          message: "message",
-          event_id: 1,
-          user_id: 1,
-        });
-      const update = await req(server)
         .put('/api/comments/1')
         .send({
           id: 1,
@@ -111,15 +82,6 @@ describe("The Comments Router", () => {
   describe("DELETE /:id", () => {
     it("should delete comment by id", async () => {
       const res = await req(server)
-        .post("/api/comments/event")
-        .send({
-          id: 1,
-          time: "123",
-          message: "message",
-          event_id: 1,
-          user_id: 1,
-        });
-      const del = await req(server)
         .del('/api/comments/1');
       expect(res.status).toBe(201);
     });
