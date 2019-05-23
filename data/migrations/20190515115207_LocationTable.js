d3336fafd0f1181a4b130809a3d269324ab58ee1
unknown <noah.stoeckel@gmail.com>
2019-05-22 21:40:10 -0400

    Adds testing stuff

--- data/migrations/20190515115207_LocationTable.js
+++ data/migrations/20190515115207_LocationTable.js
@@ -6,5 +6,5 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists("users");
+  return knex.schema.dropTableIfExists("locations");
 };
