fb55bc37ed36cbc994fceaa65aa9beefd810c534
unknown <noah.stoeckel@gmail.com>
2019-05-25 18:09:31 -0400

    Adds checks for no users, no tests

--- api/users/userRoutes.js
+++ api/users/userRoutes.js
@@ -47,17 +47,15 @@ router.post("/", async (req, res) => {
 router.put("/:id", async (req, res) => {
   const { id } = req.params;
   const user = req.body;
-  if (
-    !user.email ||
-    !user.firebase_uid ||
-    !user.username ||
-    !user.first_name ||
-    !user.last_name
-  ) {
+  const { email, firebase_uid, username, first_name, last_name } = user;
+  if (!email || !firebase_uid || !username || !first_name || !last_name) {
     res.status(400).json({ error: "Bad Request, please include all fields" });
   }
   try {
     const updatedUser = await Users.update(id, user);
+    if (!updatedUser) {
+      res.status(404).json({ error: `user with id ${id} does not exist` });
+    }
     res.status(200).json(updatedUser);
   } catch (err) {
     console.log(err);
@@ -69,6 +67,9 @@ router.delete("/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const user = await Users.remove(id);
+    if (!user) {
+      res.status(404).json({ error: `user with id ${id} does not exist` });
+    }
     res.status(200).json({ message: "User deleted" });
   } catch (error) {
     res.status(500).json(error);
