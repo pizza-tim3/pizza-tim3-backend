4261457ec53d90635f6454da9c8d4e8ec251ac2c
Taylorbelk1 <belktaylor12@gmail.com>
2019-05-30 22:05:16 -0500

    added logic to users seed file to also seed firebase_uid mocks

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,11 +1,13 @@
 const faker = require("faker");
+let count = 0;
+let fuid = 'jNpViqXD4DXmf9H2FbkQnAy'
 
 // Create Fake User Functions that returns an object (one user)
-const createFakeUser = () => ({
+const createFakeUser = (id) => ({
   email: faker.internet.email(),
   first_name: faker.name.firstName(),
   last_name: faker.name.lastName(),
-  // firebase_uid: faker.name.lastName(),
+  firebase_uid: id,
 });
 
 exports.seed = async function(knex, Promise) {
@@ -13,12 +15,18 @@ exports.seed = async function(knex, Promise) {
   let users = [];
   for (let y = 0; y < 5; y++) {
     for (let i = 0; i <= 100; i++) {
-      users.push(createFakeUser());
+      count = i;
+      let countString = count.toString();
+      let newFuid = fuid + countString;
+
+      if(newFuid.length > 28) newFuid = newFuid.slice(0, 27);
+      if(newFuid.length < 28) newFuid = newFuid.padEnd(28, '0');
+
+      users.push(createFakeUser(newFuid));
     }
     console.log(users);
     await knex("users").insert(users);
     users = [];
-    //return knex("users").insert(users);
     // Insert the array to users table
   }
 };
