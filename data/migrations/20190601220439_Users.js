34cffddfd980e21c134e1d5311af0ba866c9ac02
unknown <noah.stoeckel@gmail.com>
2019-06-02 15:39:47 -0400

    extend user info

--- data/migrations/20190601220439_Users.js
+++ data/migrations/20190601220439_Users.js
@@ -6,6 +6,10 @@ exports.up = function(knex, Promise) {
     users.text("username", 255);
     users.text("first_name", 255).notNullable();
     users.text("last_name", 255).notNullable();
+    users.text("avatar", 2048);
+    users.text("crust", 128);
+    users.text("topping", 128);
+    users.text("slices", 128);
   });
 };
 
