0d1ff11d5f836fffed890c2f512d054e13396920
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-06-01 22:27:04 -0400

    fix String old

--- data/migrations/20190601220439_Users.js
+++ data/migrations/20190601220439_Users.js
@@ -1,11 +1,11 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("users", function(users) {
     users.increments();
-    users.varchar("email", 25).notNullable();
+    users.varchar("email", 255).notNullable();
     users.text("firebase_uid");
-    users.text("username", 25);
-    users.text("first_name", 25).notNullable();
-    users.text("last_name", 25).notNullable();
+    users.text("username", 255);
+    users.text("first_name", 255).notNullable();
+    users.text("last_name", 255).notNullable();
   });
 };
 
