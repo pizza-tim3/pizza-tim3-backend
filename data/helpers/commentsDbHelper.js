96b2f5c214be63ffc8b4a1d57666a33512ed7a1d
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-06-02 18:42:56 -0400

    add get all events comments db helper, update events details response object with new comments array

--- data/helpers/commentsDbHelper.js
+++ data/helpers/commentsDbHelper.js
@@ -6,32 +6,37 @@ module.exports = {
   add,
   update,
   remove,
+  getEventAllComments,
 };
 
 function getAll() {
   return db("comments");
-};
+}
 
 function getByEvent(event_id) {
   return db("comments")
     .where({ event_id })
     .first();
-};
+}
+
+function getEventAllComments(event_id) {
+  return db("comments").where({ event_id });
+}
 
 async function add(comment, event_id) {
   return db("comments")
     .insert(comment, "event_id")
     .where({ event_id });
-};
+}
 
 async function update(id, changes) {
   return db("comments")
     .where({ id })
     .update(changes);
-};
+}
 
 async function remove(id) {
   return db("comments")
     .where({ id })
     .del();
-};
+}
