42506f54b29a3081caefdde6d3967e107c0d6a60
unknown <noah.stoeckel@gmail.com>
2019-06-04 20:02:11 -0400

    add  favoritess helpers

---
+++ data/helpers/favoritesDbHelper.js
@@ -0,0 +1,50 @@
+const db = require("../dbConfig.js");
+
+module.exports = {
+  getAll,
+  getById,
+  getByUid,
+  add,
+  update,
+  remove
+};
+
+function getAll() {
+  return db("favorites");
+}
+
+function getById(id) {
+  return db("favorites")
+    .where({ id })
+    .first();
+}
+
+function getByUid(uid) {
+  return db("favorites")
+    .where({ firebase_uid: uid })
+    .first();
+}
+
+async function add(favorite) {
+  console.log(favorite);
+  return await db("favorites")
+    .insert(favorite, "id")
+    .then(ids => {
+      return getById(ids[0]);
+    });
+}
+
+async function update(uid, changes) {
+  return await db("favorites")
+    .where({ firebase_uid: uid })
+    .update(changes, "id")
+    .then(id => {
+      return getById(id);
+    });
+}
+
+async function remove(uid) {
+  return db("favorites")
+    .where({ firebase_uid: uid })
+    .del();
+}
