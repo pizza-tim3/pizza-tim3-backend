7e83facd633a8a31d55adf24d04a8d1ad0e40162
unknown <noah.stoeckel@gmail.com>
2019-06-25 19:44:22 -0400

    add verified function to user routes

--- api/users/userRoutes.js
+++ api/users/userRoutes.js
@@ -8,14 +8,7 @@ const {
   checkAdmin
 } = require("../../auth/firebase-middleware");
 // All Users route
-
-router.use(
-  "/:user_uid",
-  verifyToken,
-  setDecodedToken,
-  setCustomClaims,
-  checkAdmin
-);
+const isRequestFromUser = require("../../utils/isRequestFromUser");
 
 router.get(
   "/",
@@ -68,38 +61,50 @@ router.post("/", async (req, res) => {
 
 router.put("/:user_uid", async (req, res) => {
   const { user_uid } = req.params;
-  const user = req.body;
-  const { email, firebase_uid, username, first_name, last_name } = user;
-  if (!email || !firebase_uid || !username || !first_name || !last_name) {
-    res.status(400).json({ error: "Bad Request, please include all fields" });
+  const verified = isRequestFromUser(req.uid, user_uid);
+  //if the user making this request is not the user in the url
+  if (!verified) {
+    res.status(403).json({ message: "unauthorized" });
   } else {
-    try {
-      const updatedUser = await Users.update(user_uid, user);
-      if (!updatedUser) {
-        res
-          .status(404)
-          .json({ error: `user with id ${user_uid} does not exist` });
+    const user = req.body;
+    const { email, firebase_uid, username, first_name, last_name } = user;
+    if (!email || !firebase_uid || !username || !first_name || !last_name) {
+      res.status(400).json({ error: "Bad Request, please include all fields" });
+    } else {
+      try {
+        const updatedUser = await Users.update(user_uid, user);
+        if (!updatedUser) {
+          res
+            .status(404)
+            .json({ error: `user with id ${user_uid} does not exist` });
+        }
+        res.status(200).json(updatedUser);
+      } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
       }
-      res.status(200).json(updatedUser);
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
     }
   }
 });
 
 router.delete("/:user_uid", async (req, res) => {
   const { user_uid } = req.params;
-  try {
-    const user = await Users.remove(user_uid);
-    if (!user) {
-      res
-        .status(404)
-        .json({ error: `user with id ${user_uid} does not exist` });
+  //if the user making this request is not the user in the url
+  const verified = isRequestFromUser(req.uid, user_uid);
+  if (!verified) {
+    res.status(403).json({ message: "unauthorized" });
+  } else {
+    try {
+      const user = await Users.remove(user_uid);
+      if (!user) {
+        res
+          .status(404)
+          .json({ error: `user with id ${user_uid} does not exist` });
+      }
+      res.status(200).json({ message: "User deleted" });
+    } catch (error) {
+      res.status(500).json(error);
     }
-    res.status(200).json({ message: "User deleted" });
-  } catch (error) {
-    res.status(500).json(error);
   }
 });
 
