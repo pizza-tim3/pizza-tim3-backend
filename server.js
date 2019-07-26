1af2bf2657cd125c0071a2ca66bcc98c109b089b
tbelk12 <taylor.belk@americannational.com>
2019-07-26 14:52:59 -0500

    test

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ server.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization");
   next();
 },
   express.json(), helmet(), logger("dev")
